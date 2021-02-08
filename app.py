b3f9f231ca6dccf96731c1dbd4cd7e4d500a2dde
Evgeny Gladkih <eogladkih@hotmaul.com>
2021-02-08 22:13:11 +0300

    add renders for template htmls

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'здесь будет главная'
+    return render_template("index.html")
 
 
 @app.route('/all')
 def all():
-    return 'здесь будут преподаватели'
+    return render_template('all.html')
 
 
 @app.route('/goals/<goal>/')
 def goals(goal):
-    return 'здесь будет цель <goal>'
+    return render_template('goal.html')
 
 
 @app.route('/profiles/<id_teacher>/')
 def profiles(id_teacher):
-    return 'здесь будет преподаватель <id учителя>'
+    return render_template('profile.html')
 
 
 @app.route('/request/')
 def request():
-    return 'здесь будет заявка на подбор,'
+    return render_template('request.html')
 
 
 @app.route('/request_done/')
 def request_done():
-    return 'заявка на подбор отправлена'
+    return render_template('request_done.html')
 
 
 @app.route('/booking/<id_teacher>/<dw>/<time>/')
 def booking_form():
-    return 'здесь будет форма бронирования <id учителя>'
+    return render_template('booking.html')
 
 
 @app.route('/booking_done/')
 def booking_done():
-    return 'заявка отправлена'
+    return render_template('booking_done.html')
 
 
 if __name__ == "__main__":
