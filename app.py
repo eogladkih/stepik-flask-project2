ba7617fe3c8217ce4dd4f2cc0277f3c7be7c7675
Evgeny Gladkih <eogladkih@hotmaul.com>
2021-02-12 19:24:04 +0300

    fix profile and booking pages

--- app.py
+++ app.py
@@ -1,6 +1,63 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+
+with open('teachers.json', 'r') as t:
+    teachers = json.load(t)
+
+with open('goals.json', 'r') as t:
+    goals_dict = json.load(t)
+
+day_dict = {"mon": "Понедельник",
+            "tue": "Вторник",
+            "wed": "Среда",
+            "thu": "Четверг",
+            "fri": "Пятница",
+            "sat": "Суббота",
+            "sun": "Воскресенье"}
+
+# print(goals['relocate'])
+
+def t_free(teacher_id):
+
+    free_time = {}
+    for en_day, ru_day in day_dict.items():
+        free_td = []
+        for item in teachers:
+            if item['id'] == teacher_id:
+                for k, v in item['free'][en_day].items():
+                    if v:
+                        free_td .append(k)
+                #free_time[ru_day] = free_td
+                free_time[en_day] = free_td
+    return free_time
+
+
+def getteacher(id):
+    for item in teachers:
+        if item['id'] == id:
+            return item
+    else:
+        return False
+
+
+
+
+
+# print(t_free(1))
+
+# def get_goals(teacher_id):
+#     for item in teachers:
+#         if item['id'] == teacher_id:
+#             goal_list = []
+#             for i in item['goals']:
+#                 goal_list.append(goals[i])
+#             return goal_list
+#
+#
+# print(get_goals(0))
 
 
 @app.route('/')
@@ -18,9 +75,13 @@ def goals(goal):
     return render_template('goal.html')
 
 
-@app.route('/profiles/<id_teacher>/')
+@app.route('/profiles/<int:id_teacher>/')
 def profiles(id_teacher):
-    return render_template('profile.html')
+    if getteacher(id_teacher):
+        item = getteacher(id_teacher)
+        free_time = t_free(id_teacher)
+        return render_template('profile.html', data=item, free_time=free_time, day_dict=day_dict, goals_dict=goals_dict)
+    return render_template('page_not_found.html'), 404
 
 
 @app.route('/request/')
@@ -33,9 +94,10 @@ def request_done():
     return render_template('request_done.html')
 
 
-@app.route('/booking/<id_teacher>/<dw>/<time>/')
-def booking_form():
-    return render_template('booking.html')
+@app.route('/booking/<int:id_teacher>/<string:dw>/<time>/')
+def booking_form(id_teacher, dw, time):
+    data = getteacher(id_teacher)
+    return render_template('booking.html', data=data, dw=dw, time=time, day_dict=day_dict, id=id_teacher )
 
 
 @app.route('/booking_done/')
