b7b7db20eed9e27f15ccf21d15161406316921d2
Evgeny Gladkih <eogladkih@hotmaul.com>
2021-02-15 21:48:55 +0300

    bulk page fixes

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request as flask_req, redirect, url_for
 import json
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField
+import random
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'sd;lfjsalfh;sajf;ldsa;cdsa;flsmc.,xzlksahkflh'
 
 with open('teachers.json', 'r') as t:
     teachers = json.load(t)
@@ -18,7 +21,6 @@ day_dict = {"mon": "Понедельник",
             "sat": "Суббота",
             "sun": "Воскресенье"}
 
-# print(goals['relocate'])
 
 def t_free(teacher_id):
 
@@ -30,7 +32,6 @@ def t_free(teacher_id):
                 for k, v in item['free'][en_day].items():
                     if v:
                         free_td .append(k)
-                #free_time[ru_day] = free_td
                 free_time[en_day] = free_td
     return free_time
 
@@ -43,36 +44,50 @@ def getteacher(id):
         return False
 
 
+def getbygoal(goal):
+    getbygoal_list = []
+    for item in teachers:
+        if goal in item['goals']:
+            getbygoal_list.append(item)
+    return getbygoal_list
+
+def getRndTeachers():
+    random6 = random.choices(teachers, k=6)
+    return random6
 
 
+class OrderForm(FlaskForm):
+    clientName = StringField('Вас зовут')
+    clientPhone = StringField('Ваш телефон')
+    clientSubmit = SubmitField('Записаться на пробный урок')
 
-# print(t_free(1))
 
-# def get_goals(teacher_id):
-#     for item in teachers:
-#         if item['id'] == teacher_id:
-#             goal_list = []
-#             for i in item['goals']:
-#                 goal_list.append(goals[i])
-#             return goal_list
-#
-#
-# print(get_goals(0))
+class RequestForm(FlaskForm):
+    clientPurpose = RadioField('clientPurpose', choices=[("travel", "Для путешествий"), ("study", "Для школы"),
+                                                         ("work", "Для работы"), ("relocate", "Для переезда")])
+    clientTime = RadioField('clientTime', choices=[("1-2", "1-2 часа в неделю"), ("3-5", "3-5 часа в неделю"),
+                                                   ("5-7", "5-7 часа в неделю"), ("7-10", "7-10 часа в неделю")])
+    clientName = StringField('Вас зовут')
+    clientPhone = StringField('Ваш телефон')
+    clientSubmit = SubmitField('Найдите мне преподавателя')
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    data = getRndTeachers()
+    return render_template("index.html", data=data)
 
 
 @app.route('/all')
 def all():
-    return render_template('all.html')
+    teachers_count = len(teachers)
+    return render_template('all.html', data=teachers, num=teachers_count)
 
 
 @app.route('/goals/<goal>/')
 def goals(goal):
-    return render_template('goal.html')
+    data = getbygoal(goal)
+    return render_template('goal.html', data=data, goal=goal, goals_dict=goals_dict)
 
 
 @app.route('/profiles/<int:id_teacher>/')
@@ -86,23 +101,27 @@ def profiles(id_teacher):
 
 @app.route('/request/')
 def request():
-    return render_template('request.html')
+    form = RequestForm()
+    return render_template('request.html', form=form)
 
 
-@app.route('/request_done/')
+@app.route('/request_done/', methods=['GET', 'POST'])
 def request_done():
-    return render_template('request_done.html')
+    if flask_req.method == 'POST':
+        return render_template('request_done.html')
 
 
 @app.route('/booking/<int:id_teacher>/<string:dw>/<time>/')
 def booking_form(id_teacher, dw, time):
     data = getteacher(id_teacher)
-    return render_template('booking.html', data=data, dw=dw, time=time, day_dict=day_dict, id=id_teacher )
+    form = OrderForm()
+    return render_template('booking.html', data=data, dw=dw, time=time, day_dict=day_dict, id=id_teacher, form=form)
 
 
-@app.route('/booking_done/')
+@app.route('/booking_done/', methods=["GET", "POST"])
 def booking_done():
-    return render_template('booking_done.html')
+    if flask_req.method == 'POST':
+        return render_template('booking_done.html')
 
 
 if __name__ == "__main__":
